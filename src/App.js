4ae2d9747e9d0505339e8ca2f77670c03c9f4205
yujinkim11 <wowyj26@naver.com>
2022-06-27 13:48:58 +0900

    router setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
+import { Blog } from "./pages/blog/Blog";
+import { Cs } from "./pages/cs/Cs";
+import { Double } from "./pages/double/Double";
+import { Introduce } from "./pages/introduce/Introduce";
+import { Product } from "./pages/product/Product";
+import { Shop } from "./pages/shop/Shop";
+import { NotFound } from "./pages/NotFound";
+import { HelmetProvider } from "react-helmet-async";
+import { GlobalStyled } from "./styles/globalStyle";
 
 function App() {
-  return <Home />;
+  return (
+    <HelmetProvider>
+      <Router>
+        <GlobalStyled />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/cs" element={<Cs />} />
+          <Route path="/double" element={<Double />} />
+          <Route path="/introduce" element={<Introduce />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyled = createGlobalStyle`
+    ${reset}
+    *{
+        box-sizing: border-box;
+    }
+`;
